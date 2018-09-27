aad05e032ff68e385989ca59e5b72380b378b245
DESKTOP-MHS2LG9\PCBDesign <thiendong.iuh@gmail.com>
2018-09-27 23:47:01 +0700

    add logo edit for each child content

--- process.js
+++ process.js
@@ -2,7 +2,7 @@ child_col1_num = 0;
 var a, id_;
 var is_select = false;
 function child_press(e){
-  e.target.innerHTML = 'Change';
+  //e.target.innerHTML = 'Change';
   a = e.target;
   id_ = e.target.id;
   is_select = true;
@@ -11,16 +11,30 @@ function col1_press(){
   if(!is_select){
     console.log('a');
     var parent = document.getElementById("col1");
-    var divtest = document.createElement("div");
-    divtest.setAttribute('class','child noselect');
-    divtest.innerHTML = "hihi";
-    divtest.id = 'c1_child' + child_col1_num;
+    var child = document.createElement("div");
+    var childcontent = document.createElement("div");
+    var childlogo = document.createElement("div");
+
+    child.setAttribute('class','child noselect');
+    //child.setAttribute('contenteditable', 'true');
+    //child.innerHTML = "hihi";
+    child.id = 'c1_child' + child_col1_num;
+
+    childcontent.setAttribute('class','childcontent noselect');
+    childcontent.setAttribute('contenteditable', 'true');
+    childcontent.innerHTML = "Code firmware";
+
+    childlogo.setAttribute('class','childlogo fa fa-edit');
+
+    child.appendChild(childcontent);
+    child.appendChild(childlogo);
+
     child_col1_num = child_col1_num + 1;
-    parent.appendChild(divtest);
-    divtest.addEventListener('mousedown',child_press,false);
+    parent.appendChild(child);
+    child.addEventListener('mousedown',child_press,false);
 
     console.log(typeof(parent.clientHeight));
-    parent.style.height = (parent.clientHeight + 70) + 'px';
+    parent.style.height = (parent.clientHeight + 80) + 'px';
   }
 }
 var k = 10;
